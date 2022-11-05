37509b013c1790295edcc349adc9a1f476840e8c
Krishna K Lalwani <krishnakashab@gmail.com>
2022-11-05 19:13:51 +0530

    Initial Commit

---
+++ P5/NFT 2.0/Code/sketch.js
@@ -0,0 +1,18 @@
+function draw() {
+  frameRate(100)
+    angleMode(DEGREES);
+    createCanvas(400, 400);
+    noFill();
+    background(252, random(198, 252), 3);
+    translate(width / 2, height / 2);
+    strokeWeight(random(5, 12));
+    smooth();
+
+    let eye = random(50, 100);
+
+    ellipse(-75, -50, 50, eye);
+    ellipse(75, -50, 50, eye);
+    arc(0, 50, 300, 200, 360, 180);
+
+    saveCanvas("smile.jpg");
+  }
---
+++ P5/NFT 2.0/Readme.md
@@ -0,0 +1,4 @@
+Hello There
+
+I am Krishna K Lalwani and I am presenting a NFT for purchase, this is in BETA right now. I will polish it, make a smart contract and put in on the marketplace. 
+
